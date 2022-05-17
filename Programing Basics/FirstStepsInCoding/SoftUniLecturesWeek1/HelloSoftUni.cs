fa01db9526044838845e65084fd2acdbb29ab6fe
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-05-17 22:36:01 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCoding/SoftUniLecturesWeek1/HelloSoftUni.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftUniLecturesWeek1
+{
+    class HelloSoftUni
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
