e8fd4cb43635427de94dd3f7b1f52f2f105bae3b
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-05-18 19:01:28 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCoding/GardenWork/YardGreening.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GardenWork
+{
+    class YardGreening
+    {
+        static void Main(string[] args)
+        {
+            double yard = double.Parse(Console.ReadLine());
+            double price = 7.61;
+            double workPrice = yard * price;
+            double discount = 0.18 * workPrice;
+            double finalPrice = workPrice - discount;
+            Console.WriteLine($"The final price is {finalPrice} lv.");
+            Console.WriteLine($"The discount is {discount} lv.");
+        }
+    }
+}
