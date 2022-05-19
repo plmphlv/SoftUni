61a6a3e1724c165ba31326b7da78465fd1c3ebf8
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-05-19 19:18:50 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCoding/ArchitectProjects/ProjectsCreation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArchitectProjects
+{
+    class ProjectsCreation
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int projAmaount = int.Parse(Console.ReadLine());
+            int projTime = projAmaount * 3;
+            Console.WriteLine($"The architect {name} will need {projTime} hours to complete {projAmaount} project/s.");
+        }
+    }
+}
