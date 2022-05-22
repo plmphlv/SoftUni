610b02e6276d455df2a4fd805776a68ae5e5ef7f
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-05-22 09:41:36 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCoding/HelloMonica/GreetingByName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloMonica
+{
+    class GreetingByName
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ Programing Basics/FirstStepsInCoding/InchTocentimetere/InchToCentimetere.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InchTocentimetere
+{
+    class InchToCentimetere
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double cm = inch * 2.54;
+            Console.WriteLine(cm);
+        }
+    }
+}
