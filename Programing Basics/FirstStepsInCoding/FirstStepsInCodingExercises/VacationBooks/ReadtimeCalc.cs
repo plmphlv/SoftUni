191942723d3ab34b4506fa17f7b32a71519046a0
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-05-24 22:36:19 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCoding/FirstStepsInCodingExercises/VacationBooks/ReadtimeCalc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VacationBooks
+{
+    class ReadtimeCalc
+    {
+        static void Main(string[] args)
+        {
+            int numPages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int numDays = int.Parse(Console.ReadLine());
+
+            int hoursPerDay = (numPages / pagesPerHour) / numDays;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
