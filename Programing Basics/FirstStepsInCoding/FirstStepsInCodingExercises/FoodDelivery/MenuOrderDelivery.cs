e11339d06b390773b1cd3ead08a17fa5b9b5df8f
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-05-31 18:10:03 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCoding/FirstStepsInCodingExercises/FoodDelivery/MenuOrderDelivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class MenuOrderDelivery
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int vegiMenu = int.Parse(Console.ReadLine());
+
+            double menuCost = chickenMenu * 10.35 + fishMenu * 12.40 + vegiMenu * 8.15;
+            double totalCost = (menuCost * 0.20) + menuCost + 2.50;
+            Console.WriteLine(totalCost);
+        }
+    }
+}
---
+++ Programing Basics/FirstStepsInCoding/FirstStepsInCodingExercises/PaintingTheHouse/PaintingPrice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PaintingTheHouse
+{
+    class PaintingPrice
+    {
+        static void Main(string[] args)
+        {
+            int covers = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int thinner = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+
+            double matterialsCost = ((covers+2)*1.50) + ((paint+(paint*0.1)) * 14.50) + (thinner *5.00) + 0.40;
+            double totalCost = (matterialsCost * 0.3) * 8 + matterialsCost;
+            Console.WriteLine(totalCost);
+        }
+    }
+}
