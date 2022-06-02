ab879952418523409c22f1ae220e06b1a7547569
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-02 07:58:56 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCoding/FirstStepsInCodingExercises/BascketBall/EquipmentCost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BascketBall
+{
+    class EquipmentCost
+    {
+        static void Main(string[] args)
+        {
+            int trainingCost = int.Parse(Console.ReadLine());
+            double trainersCost = trainingCost - (trainingCost * 0.40);
+            double uniformCost = trainersCost - (trainersCost * 0.20);
+            double ballCost = uniformCost * 0.25;
+            double accessoriesCost = ballCost * 0.20;
+            double totaCost = trainingCost + trainersCost + uniformCost + ballCost + accessoriesCost; 
+
+            Console.WriteLine(totaCost);
+        }
+    }
+}
---
+++ Programing Basics/FirstStepsInCoding/FirstStepsInCodingExercises/DepositCalculator/DepositCalc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class DepositCalc
+    {
+        static void Main(string[] args)
+        {
+            double depSum = double.Parse(Console.ReadLine());
+            int depTime = int.Parse(Console.ReadLine());
+            double anItrst = double.Parse(Console.ReadLine())/100;
+
+            double endSum = depSum +(depTime * ((depSum * anItrst) / 12));
+            Console.WriteLine(endSum);
+        }
+    }
+}
