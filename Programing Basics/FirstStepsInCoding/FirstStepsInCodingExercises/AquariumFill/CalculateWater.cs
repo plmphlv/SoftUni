6e7c585a34fa5d490e5f21f3e2903047a164ac54
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-04 07:36:38 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCoding/FirstStepsInCodingExercises/AquariumFill/CalculateWater.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AquariumFill
+{
+    class CalculateWater
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double sandOcupation = double.Parse(Console.ReadLine()) / 100;
+            double aquariumWater = ((lenght * width * height) * 0.001) * (1 - sandOcupation);
+
+            Console.WriteLine(aquariumWater);
+        }
+    }
+}
