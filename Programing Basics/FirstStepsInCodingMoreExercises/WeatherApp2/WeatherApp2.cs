7465987c214cb7c80a88003524bb6db20b10ab07
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-08 07:39:44 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCodingMoreExercises/WeatherApp2/WeatherApp2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WeatherApp2
+{
+    class WeatherApp2
+    {
+        static void Main(string[] args)
+        {
+            double temp = double.Parse(Console.ReadLine());
+
+            if (temp >= 26.00 && temp <= 35.00)
+            {
+                Console.WriteLine("Hot");
+            }
+
+            else if (temp>=20.10 && temp<=25.90)
+            {
+                Console.WriteLine("Warm");
+            }
+
+            else if (temp >= 15.00 && temp <= 20.00)
+            {
+                Console.WriteLine("Mild");
+            }
+
+            else if (temp >= 12.00 && temp <= 14.90)
+            {
+                Console.WriteLine("Cool");
+            }
+
+            else if (temp >= 5.00 && temp <= 11.90)
+            {
+                Console.WriteLine("Cold");
+            }
+
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
---
+++ Programing Basics/FirstStepsInCodingMoreExercises/WorkShop/WorkShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WorkShop
+{
+    class WorkShop
+    {
+        static void Main(string[] args)
+        {
+            double lenght =double.Parse(Console.ReadLine())*100;
+            double width = (double.Parse(Console.ReadLine())*100)-100;
+
+            double seats = Math.Floor(width / 70) * Math.Floor(lenght / 120) - 3; //Math.Floor() връща най-близкия int който е по-малък или равен на числото в скобите
+
+            Console.WriteLine(seats);
+        }
+    }
+}
