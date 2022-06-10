0563f84ec09c5bda421f0d1696cc301572abb88b
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-10 07:57:08 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCodingMoreExercises/TriangleArea/TriArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class TriArea
+    {
+        static void Main(string[] args)
+        {
+            double a1 = double.Parse(Console.ReadLine());
+            double h1 = double.Parse(Console.ReadLine());
+            double triArea = a1 * h1 / 2;
+
+            Console.WriteLine($"{triArea:f2}");
+        }
+    }
+}
---
+++ Programing Basics/FirstStepsInCodingMoreExercises/WeatherApp1/WeatherApp1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WeatherApp1
+{
+    class WeatherApp1
+    {
+        static void Main(string[] args)
+        {
+            string weather = Console.ReadLine();
+            if (weather == "sunny")
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else if (weather == "cloudy")
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+        }
+    }
+}
