388405b3ac40ca05ba9288d40ee83644e74b987c
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-11 18:52:46 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCodingMoreExercises/HousePaint/HousePaint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HousePaint
+{
+    class HousePaint
+    {
+        static void Main(string[] args)
+        {
+            double xHeight = double.Parse(Console.ReadLine()); //височина на къщата
+            double yLenght = double.Parse(Console.ReadLine()); //дължина на страничната стена
+            double hRoofHeight = double.Parse(Console.ReadLine()); //височината на триъгълната стена
+
+            double sideWallsArea =2 * (xHeight * yLenght)  - 2 * 2.25;
+            double backWallAndFrontWall = 2*(xHeight*xHeight) - 2.4;
+            double allWallsGreen = (sideWallsArea + backWallAndFrontWall)/3.4;
+
+            double roofSideWalls = 2 * ((xHeight * hRoofHeight) / 2);
+            double roofPaintRed = (2 * (xHeight * yLenght) + roofSideWalls) / 4.3;
+
+            Console.WriteLine($"{allWallsGreen:f2}");
+            Console.WriteLine($"{roofPaintRed:f2}");
+
+        }
+    }
+}
---
+++ Programing Basics/FirstStepsInCodingMoreExercises/TrapeziodArea/Area.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TrapeziodArea
+{
+    class Area
+    {
+        static void Main(string[] args)
+        {
+            double b1 = double.Parse(Console.ReadLine());
+            double b2 = double.Parse(Console.ReadLine());
+            double h1 = double.Parse(Console.ReadLine());
+
+            double area = (b1 + b2) * h1 / 2;
+
+            //как се форматира текст за да изписва само два символа след десетичната запетая
+            Console.WriteLine($"{area:0.00}");
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
