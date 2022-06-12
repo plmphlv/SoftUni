5047f3b50b4a2214db884d6df16e04f0e9023920
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-12 09:03:16 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCodingMoreExercises/FarmerProfit/FarmerProfit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FarmerProfit
+{
+    class FarmerProfit
+    {
+        static void Main(string[] args)
+        {
+            double veggiePrice = double.Parse(Console.ReadLine());
+            double fruitPrice = double.Parse(Console.ReadLine());
+            int veggieKilo = int.Parse(Console.ReadLine());
+            int fruitKilo = int.Parse(Console.ReadLine());
+            double euro = (veggiePrice * veggieKilo + fruitPrice * fruitKilo) / 1.94;
+
+
+
+            Console.WriteLine($"{euro:0.00}");
+        }
+    }
+}
---
+++ Programing Basics/FirstStepsInCodingMoreExercises/Fishland/Fisherman.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skumriya = double.Parse(Console.ReadLine()); // цена за килограм скумрия
+            double caca = double.Parse(Console.ReadLine()); //цена за килограм цаца
+            double palamud = double.Parse(Console.ReadLine()); // килограми за килограм паламуд
+            double safrid = double.Parse(Console.ReadLine()); // килограм сафрид
+            int clams = int.Parse(Console.ReadLine()); // килограм миди
+
+            double cenaPalamud = (skumriya + skumriya * 0.6)*palamud;
+            double cenaSafrid = (caca + caca * 0.8)* safrid;
+            double cenaClams = clams * 7.50;
+
+            double total = cenaPalamud + cenaSafrid + cenaClams;
+
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
