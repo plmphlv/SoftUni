2cad4586827e28047bb1d15ab73d4cf05ef9d6cd
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-13 08:21:49 +0300

    Add files via upload

---
+++ Programing Basics/FirstStepsInCodingMoreExercises/CelsiusToFahrenheit/TempConvertor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit
+{
+    class TempConvertor
+    {
+        static void Main(string[] args)
+        {
+            double celceus = double.Parse(Console.ReadLine());
+            double farenheight = celceus * 1.8 + 32;
+            Console.WriteLine($"{farenheight:f2}");
+        }
+    }
+}
---
+++ Programing Basics/FirstStepsInCodingMoreExercises/CircleArea/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CircleArea
+{
+    class Circle
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            double perimeter = 2 * Math.PI * radius;
+
+            Console.WriteLine($"{area:f2}");
+            Console.WriteLine($"{perimeter:f2}");
+        }
+    }
+}
