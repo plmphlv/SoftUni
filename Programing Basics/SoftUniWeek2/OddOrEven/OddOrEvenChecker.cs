afc14598cc1c04eb96d8d1921efe48abd2d662c0
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-18 09:39:27 +0300

    Add files via upload

---
+++ Programing Basics/SoftUniWeek2/OddOrEven/OddOrEvenChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OddOrEven
+{
+    class OddOrEvenChecker
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if(num%2==0)
+            {
+                Console.WriteLine("even");
+            }
+            else 
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
---
+++ Programing Basics/SoftUniWeek2/PasswordChecker/Password.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Password
+    {
+        static void Main(string[] args)
+        {
+            string password = "s3cr3t!P@ssw0rd";
+            string passwordInput = Console.ReadLine();
+            if (password==passwordInput)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
