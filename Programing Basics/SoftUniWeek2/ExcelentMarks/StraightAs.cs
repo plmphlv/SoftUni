b7d3b694cba7226c66c21386506b2a15c5acf4c1
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-19 10:00:09 +0300

    Add files via upload

---
+++ Programing Basics/SoftUniWeek2/ExcelentMarks/StraightAs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExcelentMarks
+{
+    class StraightAs
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            
+        }
+    }
+}
---
+++ Programing Basics/SoftUniWeek2/FigureAreas/MenuApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FigureAreas
+{
+    class MenuApp
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure== "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                double area= (side * height)/2;
+                Console.WriteLine($"{area:f3}");
+            }
+
+            if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double area = Math.Pow(radius,2)*Math.PI;
+                Console.WriteLine($"{area:f3}");
+            }
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double area = side * side;
+                Console.WriteLine($"{area:f3}");
+            }
+
+            if (figure == "rectangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                double area = side * height;
+                Console.WriteLine($"{area:f3}");
+            }
+        }
+    }
+}
