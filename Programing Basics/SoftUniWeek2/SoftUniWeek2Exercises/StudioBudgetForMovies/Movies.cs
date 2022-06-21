f65669c5602292f4d9a763e0f12483345f52bccd
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-21 08:29:06 +0300

    Add files via upload

---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2Exercises/StudioBudgetForMovies/Movies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StudioBudgetForMovies
+{
+    internal class Movies
+    {
+        static void Main(string[] args)
+        {
+            double movieBudget = double.Parse(Console.ReadLine());
+            int stuntmenCount = int.Parse(Console.ReadLine());
+            double costumePrice = double.Parse(Console.ReadLine());
+
+            double directorFee = movieBudget * 0.10;
+            double totalCostumesPrice = stuntmenCount * costumePrice;
+
+            if (stuntmenCount>150)
+            {
+                totalCostumesPrice = totalCostumesPrice - (totalCostumesPrice * 0.10);
+            }
+
+            double budgetNeeded = (directorFee + totalCostumesPrice);
+
+            if (budgetNeeded>movieBudget)
+            {
+                budgetNeeded -= movieBudget;
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {budgetNeeded:f2} leva more.");
+            }
+            else
+            {
+                movieBudget -= budgetNeeded;
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {movieBudget:f2} leva left.");
+            }
+        }
+    }
+}
---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2Exercises/SwimingWorldRecord/WorldRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwimingWorldRecord
+{
+    internal class WorldRecord
+    {
+        static void Main(string[] args)
+        {
+            double recordTime = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double estimatedTime = distance * timeForOneMeter;
+            double slowdownTime = Math.Floor((distance / 15)) * 12.5;
+            double bestTime = estimatedTime + slowdownTime;
+
+            if (bestTime<recordTime)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {bestTime:f2} seconds.");
+            }
+            else
+            {
+                bestTime -= recordTime;
+                Console.WriteLine($"No, he failed! He was {bestTime:f2} seconds slower.");
+            }
+        }
+    }
+}
