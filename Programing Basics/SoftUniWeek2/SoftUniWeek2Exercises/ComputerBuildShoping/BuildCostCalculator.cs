1501762bdfbb9c440f7c34d9cb150f61fab2060c
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-29 23:01:49 +0300

    Add files via upload

---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2Exercises/ComputerBuildShoping/BuildCostCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ComputerBuildShoping
+{
+    internal class BuildCostCalculator
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int graphicsCards = int.Parse(Console.ReadLine());
+            int intelProcessors = int.Parse(Console.ReadLine());
+            int ramSticks = int.Parse(Console.ReadLine());
+
+            double gpuCost = graphicsCards * 250;
+            double cpuCost = (gpuCost * 0.35) * intelProcessors;
+            double ramCost = (gpuCost * 0.10) * ramSticks;
+            double buildCost = gpuCost + cpuCost + ramCost;
+
+            if (graphicsCards>intelProcessors)
+            {
+                buildCost = buildCost - (buildCost * 0.15);
+            }
+
+            if (buildCost<=budget)
+            {
+                budget = budget - buildCost;
+                Console.WriteLine($"You have {budget:f2} leva left!");
+            }
+            else
+            {
+                buildCost = buildCost - budget;
+                Console.WriteLine($"Not enough money! You need {buildCost:f2} leva more!");
+            }
+        }
+    }
+}
---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2Exercises/LunchBreak/BreakTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LunchBreak
+{
+    internal class BreakTime
+    {
+        static void Main(string[] args)
+        {
+            string tvShowName = Console.ReadLine();
+            int episodeLenght = int.Parse(Console.ReadLine());
+            int breakTime = int.Parse(Console.ReadLine());
+
+            double lunchTime = breakTime * 0.125;
+            double restTime = breakTime * 0.25;
+            double timeLeft = breakTime - (lunchTime + restTime);
+
+            if (timeLeft >= episodeLenght)
+            {
+                timeLeft =Math.Ceiling(breakTime - (lunchTime + restTime+episodeLenght));
+                Console.WriteLine($"You have enough time to watch {tvShowName} and left with {timeLeft} minutes free time.");
+            }
+            else
+            {
+                timeLeft = Math.Ceiling( episodeLenght - (breakTime - (lunchTime + restTime)));
+                Console.WriteLine($"You don't have enough time to watch {tvShowName}, you need {timeLeft} more minutes.");
+            }
+        }
+    }
+}
