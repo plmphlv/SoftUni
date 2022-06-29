cc2e67ab88d9e7a7529ddc05e74db0dac99b1c86
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-06-29 23:00:46 +0300

    Add files via upload

---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2Exercises/SecondsSum/Seconds.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SecondsSum
+{
+    internal class Seconds
+    {
+        static void Main(string[] args)
+        {
+            int timeOne = int.Parse(Console.ReadLine());
+            int timeTwo = int.Parse(Console.ReadLine());
+            int timeThree = int.Parse(Console.ReadLine());
+            
+            int totalSeconds = timeOne + timeTwo + timeThree;
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+
+            //Console.WriteLine($"{minutes}:{seconds:d2}");
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}
