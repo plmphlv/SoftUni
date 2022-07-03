da843729c19d140e12895f139ea8823b843e4425
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-03 12:22:57 +0300

    Add files via upload

---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2MoreExercises/ProjectTime/TimeCalc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjectTime
+{
+    internal class TimeCalc
+    {
+        static void Main(string[] args)
+        {
+            double hoursRequired = double.Parse(Console.ReadLine());
+            int daysGiven = int.Parse(Console.ReadLine());
+            int overtimeWorkers = int.Parse(Console.ReadLine());
+
+            double actualWorkDays = daysGiven - (daysGiven * 0.1);
+            double overtimeHours = (daysGiven * 2) * overtimeWorkers;
+            double workedHours = Math.Floor(overtimeHours + (actualWorkDays * 8));
+
+            if (workedHours >= hoursRequired)
+            {
+                workedHours = workedHours - hoursRequired;
+                Console.WriteLine($"Yes!{workedHours} hours left.");
+            }
+            else
+            {
+                hoursRequired = hoursRequired - workedHours;
+                Console.WriteLine($"Not enough time!{hoursRequired} hours needed.");
+            }
+        }
+    }
+}
---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2MoreExercises/ShishoBakshisho/SasipahaYaTayaDurjava.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ShishoBakshisho
+{
+    internal class SasipahaYaTayadurjava
+    {
+        static void Main(string[] args)
+        {
+            int distance = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            double price = 0;
+
+            if (distance >= 100)
+            {
+                price = 0.06 * distance;
+                Console.WriteLine($"{price:f2}");
+            }
+            else if (distance >= 20)
+            {
+                price = 0.09 * distance;
+                Console.WriteLine($"{price:f2}");
+            }
+            else
+            {
+                if (timeOfDay == "day")
+                {
+                    price = 0.70 + 0.79 * distance;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (timeOfDay == "night")
+                {
+                    price = 0.70 + 0.90 * distance;
+                    Console.WriteLine($"{price:f2}");
+                }
+            }
+        }
+    }
+}
