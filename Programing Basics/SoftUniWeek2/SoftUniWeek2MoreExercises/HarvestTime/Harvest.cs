32aa02f059e7a9b1125f63bea2c6bc9259777536
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-05 08:08:17 +0300

    Add files via upload

---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2MoreExercises/HarvestTime/Harvest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HarvestTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wineYard = int.Parse(Console.ReadLine());
+            double grapesPerSqM = double.Parse(Console.ReadLine());
+            int wineNorm = int.Parse(Console.ReadLine());
+            int workersCount = int.Parse(Console.ReadLine());
+
+            double grapesTotal = (wineYard * grapesPerSqM) * 0.40;
+            double wineProduct = grapesTotal / 2.5;
+
+            if (wineProduct >= wineNorm)
+            {
+                double wineLeft = Math.Ceiling(wineProduct - wineNorm);
+                double wineSplit = Math.Ceiling(wineLeft / workersCount);
+                Console.WriteLine($"Good harvest this year! Total wine: {wineProduct} liters.");
+                Console.WriteLine($"{wineLeft} liters left -> {wineSplit} liters per person.");
+            }
+            else
+            {
+                double wineNeeded = Math.Floor(wineNorm - wineProduct);
+                Console.WriteLine($"It will be a tough winter! More {wineNeeded} liters wine needed.");
+            }
+        }
+    }
+}
---
+++ Programing Basics/SoftUniWeek2/SoftUniWeek2MoreExercises/PetFood/LonleyPets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PetFood
+{
+    internal class LonleyPets
+    {
+        static void Main(string[] args)
+        {
+            int absentDays = int.Parse(Console.ReadLine());
+            double totalPetFood = double.Parse(Console.ReadLine());        // in kilos
+            double dogFoodPerDay = double.Parse(Console.ReadLine());      // in kilos
+            double catFoodPerDay = double.Parse(Console.ReadLine());     // in kilos
+            double turtleFoodPerDay = double.Parse(Console.ReadLine()); // in kilos
+
+            double petFoorRequired = (absentDays * dogFoodPerDay) + (absentDays * catFoodPerDay) + ((absentDays * turtleFoodPerDay) / 1000);
+
+            if (petFoorRequired <= totalPetFood)
+            {
+                totalPetFood = Math.Floor(totalPetFood - petFoorRequired);
+                Console.WriteLine($"{totalPetFood} kilos of food left.");
+            }
+            else
+            {
+                petFoorRequired = Math.Ceiling(petFoorRequired - totalPetFood);
+                Console.WriteLine($"{petFoorRequired} more kilos of food are needed.");
+            }
+        }
+    }
+}
