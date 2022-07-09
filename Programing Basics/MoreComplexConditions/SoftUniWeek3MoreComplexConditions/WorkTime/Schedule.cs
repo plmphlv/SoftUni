6d3c9b8d72f54d4b4d30776b2a0e13d31d6e2043
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-09 09:15:24 +0300

    Add files via upload

---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditions/WorkTime/Schedule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace WorkTime
+{
+    internal class Schedule
+    {
+        static void Main(string[] args)
+        {
+            int workTime = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            switch (day)
+            {
+                case "Monday":
+                    if (workTime >= 10 && workTime <= 18)
+                        Console.WriteLine("open");
+                    else
+                        Console.WriteLine("closed");
+                    break;
+                case "Tuesday":
+                    if (workTime >= 10 && workTime <= 18)
+                        Console.WriteLine("open");
+                    else
+                        Console.WriteLine("closed");
+                    break;
+                case "Wednesday":
+                    if (workTime >= 10 && workTime <= 18)
+                        Console.WriteLine("open");
+                    else
+                        Console.WriteLine("closed");
+                    break;
+                case "Thursday":
+                    if (workTime >= 10 && workTime <= 18)
+                        Console.WriteLine("open");
+                    else
+                        Console.WriteLine("closed");
+                    break;
+                case "Friday":
+                    if (workTime >= 10 && workTime <= 18)
+                        Console.WriteLine("open");
+                    else
+                        Console.WriteLine("closed");
+                    break;
+                case "Saturday":
+                    if (workTime >= 10 && workTime <= 18)
+                        Console.WriteLine("open");
+                    else
+                        Console.WriteLine("closed");
+                    break;
+                default:
+                    Console.WriteLine("closed");
+                    break;
+            }
+        }
+    }
+}
