23ec407e9d657724d8f088ae261ff2bc6f7c1147
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-10 22:05:58 +0300

    Add files via upload

---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditions/Stores/PricePerCity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Stores
+{
+    internal class PricePerCity
+    {
+        static void Main(string[] args)
+        {
+            string shopItem = Console.ReadLine();
+            string city = Console.ReadLine();
+            double qty = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            switch (city)
+            {
+                case "Sofia":
+                    if (shopItem == "coffee")
+                        price = 0.5;
+                    else if (shopItem == "water")
+                        price = 0.8;
+                    else if (shopItem == "beer")
+                        price = 1.2;
+                    else if (shopItem == "sweets")
+                        price = 1.45;
+                    else if (shopItem == "peanuts")
+                        price = 1.6;
+                    price = price * qty;
+                    Console.WriteLine($"{price:f2}");
+                    break;
+                case "Plovdiv":
+                    if (shopItem == "coffee")
+                        price = 0.4;
+                    else if (shopItem == "water")
+                        price = 0.7;
+                    else if (shopItem == "beer")
+                        price = 1.15;
+                    else if (shopItem == "sweets")
+                        price = 1.30;
+                    else if (shopItem == "peanuts")
+                        price = 1.5;
+
+                    price = price * qty;
+                    Console.WriteLine($"{price:f2}");
+                    break;
+                case "Varna":
+                    if (shopItem == "coffee")
+                        price = 0.45;
+                    else if (shopItem == "water")
+                        price = 0.7;
+                    else if (shopItem == "beer")
+                        price = 1.1;
+                    else if (shopItem == "sweets")
+                        price = 1.35;
+                    else if (shopItem == "peanuts")
+                        price = 1.55;
+
+                    price = price * qty;
+                    Console.WriteLine($"{price:f2}");
+                    break;
+                default:
+                    Console.WriteLine("You are retarded!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditions/TradeCommisions/PricePerTrade.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace TradeCommisions
+{
+    internal class PricePerTrade
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double salesVolume = double.Parse(Console.ReadLine());
+            double commision = 0;
+
+            switch (city)
+            {
+                case "Sofia":
+                    if (salesVolume >= 0 && salesVolume <= 500)
+                    {
+                        commision = salesVolume * 0.05; // commision *= 0.05;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 500 && salesVolume <= 1000)
+                    {
+                        commision = salesVolume * 0.07; // commision *= 0.07;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 1000 && salesVolume <= 10000)
+                    {
+                        commision = salesVolume * 0.08; // commision *= 0.08;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 10000)
+                    {
+                        commision = salesVolume * 0.12; // commision *= 0.12
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+                case "Varna":
+                    if (salesVolume >= 0 && salesVolume <= 500)
+                    {
+                        commision = salesVolume * 0.045; // commision *= 0.045;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 500 && salesVolume <= 1000)
+                    {
+                        commision = salesVolume * 0.075; // commision *= 0.075;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 1000 && salesVolume <= 10000)
+                    {
+                        commision = salesVolume * 0.1; // commision *= 0.1;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 10000)
+                    {
+                        commision = salesVolume * 0.13; // commision *= 0.13;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+                case "Plovdiv":
+                    if (salesVolume >= 0 && salesVolume <= 500)
+                    {
+                        commision = salesVolume * 0.055; // commision *= 0.055;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 500 && salesVolume <= 1000)
+                    {
+                        commision = salesVolume * 0.08; // commision *= 0.08;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 1000 && salesVolume <= 10000)
+                    {
+                        commision = salesVolume * 0.12; // commision *= 0.12;
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else if (salesVolume > 10000)
+                    {
+                        commision = salesVolume * 0.145; // commision *= 0.145
+                        Console.WriteLine($"{commision:f2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+
+            }
+        }
+    }
+}
