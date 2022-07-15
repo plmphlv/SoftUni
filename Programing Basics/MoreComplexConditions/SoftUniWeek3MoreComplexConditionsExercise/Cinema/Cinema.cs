dd4b551033fee9233842d8264b45e24804625051
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-15 19:04:57 +0300

    Add files via upload

---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsExercise/Cinema/Cinema.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cinema
+{
+    internal class Cinema
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            double income = 0;
+
+            income = rows * columns;
+
+            if (projection == "Premiere")
+            {
+                income = rows * columns * 12;
+            }
+            else if (projection == "Normal")
+            {
+                income = rows * columns * 7.50;
+            }
+            else if (projection == "Discount")
+            {
+                income = rows * columns * 5;
+            }
+
+            Console.WriteLine($"{income:f2}");
+        }
+    }
+}
