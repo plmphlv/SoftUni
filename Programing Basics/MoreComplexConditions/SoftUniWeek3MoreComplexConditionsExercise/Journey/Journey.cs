e19f8dda03082590855f97c811cf05ae3fc0f046
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-18 18:03:37 +0300

    Add files via upload

---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsExercise/Journey/Journey.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Journey
+{
+    internal class Journey
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            if (budget <= 100)
+            {
+
+                if (season == "summer")
+                {
+                    budget = budget * 0.3;
+                    Console.WriteLine("Somewhere in Bulgaria");
+                    Console.WriteLine($"Camp - {budget:f2}");
+
+                }
+                else if (season == "winter")
+                {
+                    budget = budget * 0.7;
+                    Console.WriteLine("Somewhere in Bulgaria");
+                    Console.WriteLine($"Hotel - {budget:f2}");
+                }
+            }
+            else if (budget <= 1000)
+            {
+                if (season == "summer")
+                {
+                    budget = budget * 0.4;
+                    Console.WriteLine("Somewhere in Balkans");
+                    Console.WriteLine($"Camp - {budget:f2}");
+
+                }
+                else if (season == "winter")
+                {
+                    budget = budget * 0.8;
+                    Console.WriteLine("Somewhere in Balkans");
+                    Console.WriteLine($"Hotel - {budget:f2}");
+                }
+            }
+            else if (budget > 1000)
+            {
+                budget = budget * 0.9;
+                Console.WriteLine("Somewhere in Europe");
+                Console.WriteLine($"Hotel - {budget:f2}");
+            }
+        }
+    }
+}
---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsExercise/NewHouse/NewHouse.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace NewHouse
+{
+    internal class NewHouse
+    {
+        static void Main(string[] args)
+        {
+            string flowers = Console.ReadLine();
+            int flowerQty = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+            double finalTotal = 0;
+
+            switch (flowers)
+            {
+                case "Roses":
+                    finalTotal = flowerQty * 5;
+                    if (flowerQty > 80)
+                    {
+                        finalTotal = finalTotal - (finalTotal * 0.1);
+                    }
+                    break;
+                case "Dahlias":
+                    finalTotal = flowerQty * 3.8;
+                    if (flowerQty > 90)
+                    {
+                        finalTotal = finalTotal - (finalTotal * 0.15);
+                    }
+                    break;
+                case "Tulips":
+                    finalTotal = flowerQty * 2.8;
+                    if (flowerQty > 80)
+                    {
+                        finalTotal = finalTotal - (finalTotal * 0.15);
+                    }
+                    break;
+                case "Narcissus":
+                    finalTotal = flowerQty * 3;
+                    if (flowerQty < 120)
+                    {
+                        finalTotal = finalTotal + (finalTotal * 0.15);
+                    }
+                    break;
+                case "Gladiolus":
+                    finalTotal = flowerQty * 2.5;
+                    if (flowerQty < 80)
+                    {
+                        finalTotal = finalTotal + (finalTotal * 0.2);
+                    }
+                    break;
+            }
+            if (budget>=finalTotal)
+            {
+                finalTotal=budget-finalTotal;
+                Console.WriteLine($"Hey, you have a great garden with {flowerQty} {flowers} and {finalTotal:f2} leva left.");
+            }
+            else
+            {
+                finalTotal = finalTotal- budget;
+                Console.WriteLine($"Not enough money, you need {finalTotal:f2} leva more.");
+            }
+        }
+    }
+}
