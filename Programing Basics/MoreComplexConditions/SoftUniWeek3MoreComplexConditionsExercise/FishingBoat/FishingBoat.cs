8d9821c134aed4e482d6b2b825ce385cece54286
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-19 07:43:54 +0300

    Add files via upload

---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsExercise/FishingBoat/FishingBoat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FishingBoat
+{
+    internal class FishingBoat
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermen = int.Parse(Console.ReadLine());
+            double cost = 0;
+
+            if (season == "Spring")
+            {
+                cost = 3000;
+            }
+            else if (season == "Winter")
+            {
+                cost = 2600;
+            }
+            else if (season == "Summer" || season == "Autumn")
+            {
+                cost = 4200;
+            }
+
+            if (fishermen <= 6)
+            {
+                cost = cost - (cost * 0.1);
+            }
+            else if (fishermen >= 7 && fishermen <= 11)
+            {
+                cost = cost - (cost * 0.15);
+            }
+            else if (fishermen > 12)
+            {
+                cost = cost - (cost * 0.25);
+            }
+
+            if (fishermen % 2 == 0 && season != "Autumn")
+            {
+                cost = cost - (cost * 0.05);
+            }
+
+            if (budget >= cost)
+            {
+                Console.WriteLine($"Yes! You have {budget - cost:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {cost - budget:f2} leva.");
+            }
+        }
+    }
+}
---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsExercise/HotelRoom/HotelRoom.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HotelRoom
+{
+    internal class HotelRoom
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double apartment = 0;
+            double studio = 0;
+            double totalCostStudio = 0;
+            double totalCostApartmen = 0;
+
+            if (month == "May" || month == "October")
+            {
+                studio = 50;
+                apartment = 65;
+            }
+            else if (month == "June" || month == "September")
+            {
+                studio = 75.20;
+                apartment = 68.70;
+            }
+            else if (month == "July" || month == "August")
+            {
+                studio = 76;
+                apartment = 77;
+            }
+
+            totalCostStudio = studio * nights;
+            totalCostApartmen = apartment * nights;
+
+            if (nights > 14 && (month == "May" || month == "October"))
+            {
+                totalCostStudio = totalCostStudio - (totalCostStudio * 0.3);
+            }
+            else if (nights > 14 && (month == "June" || month == "September"))
+            {
+                totalCostStudio = totalCostStudio - (totalCostStudio * 0.2);
+            }
+            else if (nights > 7 && (month == "May" || month == "October"))
+            {
+                totalCostStudio = totalCostStudio - (totalCostStudio * 0.05);
+            }
+
+
+            if (nights > 14)
+            {
+                totalCostApartmen = totalCostApartmen - (totalCostApartmen * 0.1);
+            }
+
+            Console.WriteLine($"Apartment: {totalCostApartmen:f2} lv.");
+            Console.WriteLine($"Studio: {totalCostStudio:f2} lv.");
+        }
+    }
+}
