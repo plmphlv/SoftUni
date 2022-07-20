4e5d4356454d09f425286ff4255287641ec32281
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-20 15:28:37 +0300

    Add files via upload

---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsExercise/TruckDriver/TruckDriver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TruckDriver
+{
+    internal class TruckDriver
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            double distance = double.Parse(Console.ReadLine());
+
+            double pricePerKm = 0;
+            double earnins = 0;
+            double proffit = 0;
+
+            if (distance <= 5000)
+            {
+                if (season == "Spring" || season == "Autumn")
+                    pricePerKm = 0.75;
+                else if (season == "Summer")
+                    pricePerKm = 0.9;
+                else if (season == "Winter")
+                    pricePerKm = 1.05;
+            }
+            else if (distance > 5000 && distance <= 10000)
+            {
+                if (season == "Spring" || season == "Autumn")
+                    pricePerKm = 0.95;
+                else if (season == "Summer")
+                    pricePerKm = 1.1;
+                else if (season == "Winter")
+                    pricePerKm = 1.25;
+            }
+            else if (distance > 10000 && distance <= 20000)
+            {
+                pricePerKm = 1.45;
+            }
+
+            earnins = (pricePerKm * distance)*4;
+            proffit = earnins - (earnins * 0.1);
+
+            Console.WriteLine($"{proffit:f2}");
+
+        }
+    }
+}
---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsExercise/Vacation/Vacation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Vacation
+{
+    internal class Vacation
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string destination = string.Empty;
+            string resortType = string.Empty;
+
+            if (budget <= 1000)
+            {
+                resortType = "Camp";
+                if (season == "Summer")
+                {
+                    destination = "Alaska";
+                    budget = budget * 0.65;
+                    Console.WriteLine($"{destination} - {resortType} - {budget:f2}");
+                }
+                else if (season == "Winter")
+                {
+                    destination = "Morocco";
+                    budget = budget * 0.45;
+                    Console.WriteLine($"{destination} - {resortType} - {budget:f2}");
+                }
+            }
+            else if (budget <= 3000)
+            {
+                resortType = "Hut";
+                if (season == "Summer")
+                {
+                    destination = "Alaska";
+                    budget = budget * 0.8;
+                    Console.WriteLine($"{destination} - {resortType} - {budget:f2}");
+                }
+                else if (season == "Winter")
+                {
+                    destination = "Morocco";
+                    budget = budget * 0.6;
+                    Console.WriteLine($"{destination} - {resortType} - {budget:f2}");
+                }
+            }
+            else if (budget > 3000)
+            {
+                resortType = "Hotel";
+                budget = budget * 0.9;
+
+                if (season == "Summer")
+                {
+                    destination = "Alaska";
+                }
+                else if (season == "Winter")
+                {
+                    destination = "Morocco";
+                }
+
+                Console.WriteLine($"{destination} - {resortType} - {budget:f2}");
+            }
+        }
+    }
+}
