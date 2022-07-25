7ec08434718fb4ada23378550cad1feed1463a7e
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-25 18:49:49 +0300

    Add files via upload

---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsMoreExercises/DotInRectangle/DotInRectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DotInRectangle
+{
+    internal class DotInRectangle
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            if ((x == x1 || x == x2) && (y >= y1 && y <= y2))
+            {
+                Console.WriteLine("Border");
+            }
+            else if ((y == y1 || y == y2) && (x >= x1 && x <= x2))
+            {
+                Console.WriteLine($"Border");
+            }
+            //else if ((x > x1 && x < x1) && (y > y1 && y < y2))
+            //  Console.WriteLine("Inside");
+            else
+            {
+                Console.WriteLine("Inside / Outside");
+            }
+        }
+    }
+}
---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsMoreExercises/Flowers/Flowers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Flowers
+{
+    internal class Flowers
+    {
+        static void Main(string[] args)
+        {
+            int hrizantemi = int.Parse(Console.ReadLine());
+            int rozi = int.Parse(Console.ReadLine());
+            int laleta = int.Parse(Console.ReadLine());
+            string sezon = Console.ReadLine();
+            char praznik = char.Parse(Console.ReadLine());
+
+            double buketCena = 0;
+
+            if (sezon == "Spring" || sezon == "Summer")
+            {
+                buketCena = (hrizantemi * 2) + (rozi * 4.1) + (laleta * 2.5);
+            }
+            else if (sezon == "Autumn" || sezon == "Winter")
+            {
+                buketCena = (hrizantemi * 3.75) + (rozi * 4.50) + (laleta * 4.15);
+            }
+
+            if (praznik == 'Y')
+            {
+                buketCena = buketCena + (buketCena * 0.15);
+            }
+
+            if (laleta > 7 && sezon == "Spring")
+            {
+                buketCena = buketCena - (buketCena * 0.05);
+            }
+            if (rozi >= 10 && sezon == "Winter")
+            {
+                buketCena = buketCena - (buketCena * 0.1);
+            }
+            if (laleta + hrizantemi + rozi > 20)
+            {
+                buketCena = buketCena - (buketCena * 0.2);
+            }
+
+            Console.WriteLine($"{buketCena+2:f2}");
+        }
+    }
+}
