a3bcbc0447f45a0c7e0bdb24a777392e603b2f32
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-25 18:51:33 +0300

    Add files via upload

---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsMoreExercises/BikeRace/BikeRace.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BikeRace
+{
+    internal class BikeRace
+    {
+        static void Main(string[] args)
+        {
+            int juniors = int.Parse(Console.ReadLine());
+            int seniors = int.Parse(Console.ReadLine());
+            string raceType = Console.ReadLine();
+
+            double charityDonation = 0;
+
+            if (raceType == "trail")
+            {
+                charityDonation = (juniors * 5.5) + (seniors * 7);
+            }
+            else if (raceType == "cross-country")
+            {
+                charityDonation = (juniors * 8) + (seniors * 9.50);
+            }
+            else if (raceType == "downhill")
+            {
+                charityDonation = (juniors * 12.25) + (seniors * 13.75);
+            }
+            else if (raceType == "road")
+            {
+                charityDonation = (juniors * 20) + (seniors * 21.50);
+            }
+
+            if (juniors + seniors >= 50 && raceType == "cross-country")
+            {
+                charityDonation = charityDonation - (charityDonation * 0.25);
+            }
+
+            charityDonation = charityDonation - (charityDonation * 0.05);
+
+            Console.WriteLine($"{charityDonation:f2}");
+        }
+    }
+}
---
+++ Programing Basics/MoreComplexConditions/SoftUniWeek3MoreComplexConditionsMoreExercises/CarToGo/CarToGo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CarToGo
+{
+    internal class CarToGo
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            double rentPrice = 0;
+
+            if (budget <= 100)
+            {
+                if (season == "Summer")
+                {
+                    rentPrice = budget * 0.35;
+                    Console.WriteLine("Economy class");
+                    Console.WriteLine($"Cabrio - {rentPrice:f2}");
+                }
+                else if (season == "Winter")
+                {
+                    rentPrice = budget * 0.65;
+                    Console.WriteLine("Economy class");
+                    Console.WriteLine($"Jeep - {rentPrice:f2}");
+                }
+            }
+            else if (budget <= 500)
+            {
+                if (season == "Summer")
+                {
+                    rentPrice = budget * 0.45;
+                    Console.WriteLine("Compact class");
+                    Console.WriteLine($"Cabrio - {rentPrice:f2}");
+                }
+                else if (season == "Winter")
+                {
+                    rentPrice = budget * 0.8;
+                    Console.WriteLine("Compact class");
+                    Console.WriteLine($"Jeep - {rentPrice:f2}");
+                }
+            }
+            else if (budget > 500)
+            {
+                rentPrice = budget * 0.9;
+                Console.WriteLine("Luxury class");
+                Console.WriteLine($"Jeep - {rentPrice:f2}");
+
+            }
+        }
+    }
+}
