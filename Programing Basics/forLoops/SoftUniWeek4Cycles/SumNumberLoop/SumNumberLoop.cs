24f0df5754398db2fe95f06d82a72784367ccd43
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-26 08:27:00 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4Cycles/SumNumberLoop/SumNumberLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumNumberLoop
+{
+    internal class SumNumberLoop
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= inputCount; i++)
+            {
+                int numInput = int.Parse(Console.ReadLine());
+                sum += numInput;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Programing Basics/forLoops/SoftUniWeek4Cycles/VowelsSum/VowelsSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VowelsSum
+{
+    internal class VowelsSum
+    {
+        static void Main(string[] args)
+        {
+            string input= Console.ReadLine();
+            int sum = 0;
+            
+            for(int i = 0; i <= input.Length-1; i++)
+            {
+                switch(input[i])
+                {
+                    case 'a':
+                        sum = sum + 1;
+                        break;
+                        case 'e':
+                        sum = sum + 2;
+                        break;
+                    case 'i':
+                        sum = sum + 3;
+                        break;
+                    case 'o':
+                        sum = sum + 4;
+                        break;
+                    case 'u':
+                        sum = sum + 5;
+                        break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
