61f4685874814f7d99a4d17fd7cb07abc09620d4
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-28 08:18:43 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4Cycles/OddEvenSum/OddEvenSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    internal class OddEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int counter = 1; counter <= numberOfInputs; counter++)
+            {
+                int numInput = int.Parse(Console.ReadLine());
+
+                if (counter % 2 == 0)
+                {
+                    evenSum += numInput;
+                }
+                else
+                {
+                    oddSum += numInput;
+                }
+            }
+
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {oddSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No");
+                Console.WriteLine($"Diff = {Math.Abs(oddSum - evenSum)}");
+            }
+        }
+    }
+}
