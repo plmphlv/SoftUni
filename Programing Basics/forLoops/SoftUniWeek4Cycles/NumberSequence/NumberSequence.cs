87f3c87581ff727aea0f75627ac25d0e6f82a478
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-28 08:20:41 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4Cycles/NumberSequence/NumberSequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    internal class NumberSequence
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int minValue = int.MaxValue;
+            int maxValue = int.MinValue;
+
+            for (int i = 1; i <= count; i++)
+            {
+                int numer=int.Parse(Console.ReadLine());
+
+                if (numer <minValue)
+                {
+                    minValue = numer;
+                }
+                if (numer > maxValue)
+                {
+                    maxValue = numer;
+                }
+            }
+
+            Console.WriteLine($"Max number: {maxValue}");
+            Console.WriteLine($"Min number: {minValue}");
+        }
+    }
+}
