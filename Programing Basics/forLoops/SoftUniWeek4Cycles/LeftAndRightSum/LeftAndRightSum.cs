ac50be154cbeef3d5d95d4448e18e0f22bb30241
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-29 08:26:04 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4Cycles/LeftAndRightSum/LeftAndRightSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    internal class LeftAndRightSum
+    {
+        static void Main(string[] args)
+        {
+            int nimOfinputs = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 1; i <= 2 * nimOfinputs; i++)
+            {
+                int inputNums = int.Parse(Console.ReadLine());
+
+                if (i <=nimOfinputs)
+                {
+                    rightSum += inputNums;
+                }
+                else
+                {
+                    leftSum += inputNums;
+                }
+            }
+            if (rightSum == leftSum)
+            {
+                Console.WriteLine($"Yes, sum = {rightSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+            }
+        }
+    }
+}
