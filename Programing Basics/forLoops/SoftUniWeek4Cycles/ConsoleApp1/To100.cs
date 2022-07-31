54ddb8df56dc2c9c153866df6516d3c71578c26a
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-07-31 11:01:54 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4Cycles/ConsoleApp1/To100.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace To100
+{
+    internal class To100
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
