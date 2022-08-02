abd7c0ac7184f06a3480485351601c27b8989a02
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-02 22:10:06 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesExercises/NumbersEndingIn7/NumbersEndingIn7.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumbersEndingIn7
+{
+    internal class NumbersEndingIn7
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 7; num <= 997; num += 10)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesExercises/Oscars/Oscars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Oscars
+{
+    internal class Oscars
+    {
+        static void Main(string[] args)
+        {
+            string actor = Console.ReadLine();
+            double startingPoints = double.Parse(Console.ReadLine());
+            int numberOfJudjes = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberOfJudjes; i++)
+            {
+                string judge = Console.ReadLine();
+                double points = double.Parse(Console.ReadLine());
+
+                startingPoints = (judge.Length * points) / 2 + startingPoints;
+
+                // Console.WriteLine($"{startingPoints:f2}");
+                if (startingPoints >= 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actor} got a nominee for leading role with {startingPoints:f1}!");
+                    return;
+                }
+            }
+            if (startingPoints < 1250.5)
+            {
+                Console.WriteLine($"Sorry, {actor} you need {1250.5 - startingPoints:f1} more!");
+            }
+        }
+    }
+}
