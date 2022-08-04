0efb103aae19455c8bd19d30637ec4e7507244a8
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-04 18:27:13 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesExercises/HalfSumElement/HalfSumElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    internal class HalfSumElement
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLoops = int.Parse(Console.ReadLine());
+            int bigBoy = int.MinValue;
+            int sum = 0;
+
+            for (int i = 1; i <= numberOfLoops; i++)
+            {
+                int nums=int.Parse(Console.ReadLine());
+
+                if (nums > bigBoy)
+                {
+                    bigBoy = nums;
+                }
+
+                sum += nums;
+            }
+
+            if ((sum-bigBoy)==bigBoy)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sum - bigBoy}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs((sum - bigBoy)-bigBoy)}");
+            }
+        }
+    }
+}
---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesExercises/Histogram/Histogram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Histogram
+{
+    internal class Histogram
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+            int p4 = 0;
+            int p5 = 0;
+
+            for (int i = 1; i <= numberOfInputs; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 200)
+                    p1++;
+                else if (num >= 200 && num <= 399)
+                    p2++;
+                else if (num >= 400 && num <= 599)
+                    p3++;
+                else if (num >= 600 && num <= 799)
+                    p4++;
+                else if (num >= 800)
+                    p5++;
+            }
+
+            double percent1 = ((double)p1 / (double)numberOfInputs)*100;
+            double percent2 = ((double)p2 / (double)numberOfInputs)*100;
+            double percent3 = ((double)p3 / (double)numberOfInputs)*100;
+            double percent4 = ((double)p4 / (double)numberOfInputs)*100;
+            double percent5 = ((double)p5 / (double)numberOfInputs)*100;
+
+            Console.WriteLine($"{percent1:f2}%");
+            Console.WriteLine($"{percent2:f2}%");
+            Console.WriteLine($"{percent3:f2}%");
+            Console.WriteLine($"{percent4:f2}%");
+            Console.WriteLine($"{percent5:f2}%");
+        }
+    }
+}
