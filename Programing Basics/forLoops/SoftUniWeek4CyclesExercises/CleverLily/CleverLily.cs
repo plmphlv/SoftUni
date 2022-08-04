0354b052fd724b3fb14a0149c629d3df8e2dda08
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-04 18:27:50 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesExercises/CleverLily/CleverLily.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CleverLily
+{
+    internal class CleverLily
+    {
+        static void Main(string[] args)
+        {
+            int ageOfLily = int.Parse(Console.ReadLine());
+            double washerCost = double.Parse(Console.ReadLine());
+            int pricePerToy = int.Parse(Console.ReadLine());
+
+            int toys = 0;
+            double savings = 0;
+
+            for (int currentAge = 1; currentAge <= ageOfLily; currentAge++)
+            {
+                if (currentAge % 2 == 0)
+                {
+                    savings += (currentAge * 5) - 1;
+                }
+                else
+                {
+                    toys++;
+                }
+            }
+
+            savings += (pricePerToy * toys);
+
+            if (washerCost <= savings)
+            {
+                Console.WriteLine($"Yes! {savings - washerCost:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washerCost - savings:f2}");
+            }
+        }
+    }
+}
