84b7d3ffddfa2d173d5ae82cf7b4165bbf5d2fa1
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-07 23:09:58 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesMoreExercises/Grades/Grades.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Grades
+{
+    internal class Grades
+    {
+        static void Main(string[] args)
+        {
+            int studentCount = int.Parse(Console.ReadLine());
+
+            double gradesTotal = 0;
+
+            double excelentMarks = 0;
+            double goodMarks = 0;
+            double averageMark = 0;
+            double failMark = 0;
+
+            int topStudents = 0;
+            int goodStudents = 0;
+            int averageStudents = 0;
+            int failStudents = 0;
+
+            for (int student = 1; student <= studentCount; student++)
+            {
+                double score = double.Parse(Console.ReadLine());
+
+                if (score >= 2.00 && score <= 2.99)
+                {
+                    failMark += score;
+                    failStudents++;
+                }
+                else if (score >= 3.00 && score <= 3.99)
+                {
+                    averageMark += score;
+                    averageStudents++;
+                }
+                else if (score >= 4.00 && score <= 4.99)
+                {
+                    goodMarks += score;
+                    goodStudents++;
+                }
+                else if (score >= 5.00)
+                {
+                    excelentMarks += score;
+                    topStudents++;
+                }
+
+                gradesTotal += score;
+            }
+
+            double averageScore = gradesTotal / studentCount;
+
+            double failScore = ((double)failStudents / (double)studentCount)*100;
+            double averageStudentsScore = ((double)averageStudents / (double)studentCount)*100;
+            double goodStudentsScore = ((double)goodStudents / (double)studentCount)*100;
+            double excelentStudentsScore = ((double)topStudents / (double)studentCount)*100;
+
+            Console.WriteLine($"Top students: {excelentStudentsScore:f2}%");
+            Console.WriteLine($"Between 4.00 and 4.99: {goodStudentsScore:f2}%");
+            Console.WriteLine($"Between 3.00 and 3.99: {averageStudentsScore:f2}%");
+            Console.WriteLine($"Fail: {failScore:f2}%");
+            Console.WriteLine($"Average: {averageScore:f2}");
+        }
+    }
+}
---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesMoreExercises/Hospital/Hospital.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Hospital
+{
+    internal class Hospital
+    {
+        static void Main(string[] args)
+        {
+            int periondOfTime = int.Parse(Console.ReadLine());
+
+            int doctorCount = 7;
+            int treatedPationts = 0;
+            int untreatetPationts = 0;
+
+            for (int days = 1; days <= periondOfTime; days++)
+            {
+                if (days % 3 == 0 && untreatetPationts > treatedPationts)
+                {
+                    doctorCount++;
+                }
+
+                int patients = int.Parse(Console.ReadLine());
+
+                if (patients > doctorCount)
+                {
+                    treatedPationts += doctorCount;
+                    untreatetPationts += (patients - doctorCount);
+                }
+                else
+                {
+                    treatedPationts += patients;
+                }
+            }
+
+            Console.WriteLine($"Treated patients: {treatedPationts}.");
+            Console.WriteLine($"Untreated patients: {untreatetPationts}.");
+
+        }
+    }
+}
