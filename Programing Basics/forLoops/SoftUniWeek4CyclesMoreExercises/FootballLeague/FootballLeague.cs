05ccebb33423c0868d23e4b85188d59193f763fe
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-09 20:51:26 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesMoreExercises/FootballLeague/FootballLeague.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FootballLeague
+{
+    internal class FootballLeague
+    {
+        static void Main(string[] args)
+        {
+            int stadiumCapacity = int.Parse(Console.ReadLine());
+            int fanCount = int.Parse(Console.ReadLine());
+
+            int sectionA = 0;
+            int sectionB = 0;
+            int sectionV = 0;
+            int sectionG = 0;
+
+            for (int i = 1; i <= fanCount; i++)
+            {
+                char section = char.Parse(Console.ReadLine());
+
+                if (section == 'A')
+                    sectionA++;
+                else if (section == 'B')
+                    sectionB++;
+                else if (section == 'V')
+                    sectionV++;
+                else if (section == 'G')
+                    sectionG++;
+            }
+
+            double sectionAPercentage = ((double)sectionA / (double)fanCount) * 100;
+            double sectionBPercentage = ((double)sectionB / (double)fanCount) * 100;
+            double sectionVPercentage = ((double)sectionV / (double)fanCount) * 100;
+            double sectionGPercentage = ((double)sectionG / (double)fanCount) * 100;
+            double fanPercentage = ((double)fanCount / stadiumCapacity) * 100;
+
+            Console.WriteLine($"{sectionAPercentage:f2}%");
+            Console.WriteLine($"{sectionBPercentage:f2}%");
+            Console.WriteLine($"{sectionVPercentage:f2}%");
+            Console.WriteLine($"{sectionGPercentage:f2}%");
+            Console.WriteLine($"{fanPercentage:f2}%");
+        }
+    }
+}
---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesMoreExercises/GameOfIntervals/GameOfIntervals.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace GameOfIntervals
+{
+    internal class GameOfIntervals
+    {
+        static void Main(string[] args)
+        {
+            int turnCount = int.Parse(Console.ReadLine());
+
+            double scoreboard = 0;
+
+            int lineOne = 0;
+            int lineTwo = 0;
+            int lineThree = 0;
+            int lineFour = 0;
+            int lineFive = 0;
+            int lineInvalid = 0;
+
+            for (int i = 1; i <= turnCount; i++)
+            {
+                int score = int.Parse(Console.ReadLine());
+
+                if (score >= 0 && score <= 9)
+                {
+                    scoreboard += (double)score * 0.2;
+                    lineOne++;
+                }
+                else if (score >= 10 && score <= 19)
+                {
+                    scoreboard += (double)score * 0.3;
+                    lineTwo++;
+                }
+                else if (score >= 20 && score <= 29)
+                {
+                    scoreboard += (double)score * 0.4;
+                    lineThree++;
+                }
+                else if (score >= 30 && score <= 39)
+                {
+                    scoreboard += 50;
+                    lineFour++;
+                }
+                else if (score >= 40 && score <= 50)
+                {
+                    scoreboard += 100;
+                    lineFive++;
+                }
+                else if (score <= 0 || score > 50)
+                {
+                    scoreboard /= 2;
+                    lineInvalid++;
+                }
+            }
+
+            double lineOnePercent = (double)lineOne / (double)turnCount * 100;
+            double lineTwoPercent = (double)lineTwo / (double)turnCount * 100;
+            double lineThreePercent = (double)lineThree / (double)turnCount * 100;
+            double lineFourPercent = (double)lineFour / (double)turnCount * 100;
+            double lineFivePercent = (double)lineFive / (double)turnCount * 100;
+            double lineInvalidPercent = (double)lineInvalid / (double)turnCount * 100;
+
+            Console.WriteLine($"{scoreboard:f2}");
+            Console.WriteLine($"From 0 to 9: {lineOnePercent:f2}%");
+            Console.WriteLine($"From 10 to 19: {lineTwoPercent:f2}%");
+            Console.WriteLine($"From 20 to 29: {lineThreePercent:f2}%");
+            Console.WriteLine($"From 30 to 39: {lineFourPercent:f2}%");
+            Console.WriteLine($"From 40 to 50: {lineFivePercent:f2}%");
+            Console.WriteLine($"Invalid numbers: {lineInvalidPercent:f2}%");
+        }
+    }
+}
