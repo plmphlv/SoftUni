be991dc5732157c02124320f7f79f4fc27072f43
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-11 09:15:07 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesMoreExercises/BackToThePast/BackToThePast.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BackToThePast
+{
+    internal class BackToThePast
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int yearTarget = int.Parse(Console.ReadLine());
+
+            double moneyNeed = 0;
+            
+
+            for (int year = 1800; year <= yearTarget; year++)
+            {
+                
+                if (year % 2 == 0)
+                {
+                    moneyNeed += 12000;
+                }
+                else
+                {
+                    moneyNeed += 12000 + (50 * (18+(year-1800)));
+                }
+            }
+
+            if (money>=moneyNeed)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {money - moneyNeed:f2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {moneyNeed - money:f2} dollars to survive.");
+            }
+        }
+    }
+}
---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesMoreExercises/Bills/Bills.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Bills
+{
+    internal class Bills
+    {
+        static void Main(string[] args)
+        {
+            int timePeriod = int.Parse(Console.ReadLine());
+
+            double electricityBills = 0;
+            double waterBills = 0;
+            double internetBills = 0;
+            double miscBills = 0;
+
+            for (int i = 1; i <= timePeriod; i++)
+            {
+                double electricityPrice = double.Parse(Console.ReadLine());
+
+                electricityBills += electricityPrice;
+                waterBills += 20;
+                internetBills += 15;
+                miscBills += (electricityPrice + 20 + 15) + ((electricityPrice + 20 + 15) * 0.2);
+
+            }
+            double average = (electricityBills + waterBills + internetBills + miscBills) / timePeriod;
+
+            Console.WriteLine($"Electricity: {electricityBills:f2} lv");
+            Console.WriteLine($"Water: {waterBills:f2} lv");
+            Console.WriteLine($"Internet: {internetBills:f2} lv");
+            Console.WriteLine($"Other: {miscBills:f2} lv");
+            Console.WriteLine($"Average: {average:f2} lv");
+        }
+    }
+}
