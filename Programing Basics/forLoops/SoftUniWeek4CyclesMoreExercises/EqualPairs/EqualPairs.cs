ebc138e1f5788e0d74e9bc2eeb62c3f2e522d6a3
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-11 09:14:07 +0300

    Add files via upload

---
+++ Programing Basics/forLoops/SoftUniWeek4CyclesMoreExercises/EqualPairs/EqualPairs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EqualPairs
+{
+    internal class EqualPairs
+    {
+        static void Main(string[] args)
+        {
+            int pairs=int.Parse(Console.ReadLine());
+
+            int maxDiff = 0;
+            int store = 0;
+
+            for (int i = 1; i <= pairs; i++)
+            {
+                int num1=int.Parse(Console.ReadLine());
+                int num2=int.Parse(Console.ReadLine());
+                
+                int sum=num1 + num2;
+                
+                if(sum>=maxDiff)
+                {
+                    
+                }
+            }
+        }
+    }
+}
