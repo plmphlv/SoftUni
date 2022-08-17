38d90cc4aa31b83ea1fb3584a528f84f126cb073
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-17 18:47:23 +0300

    Add files via upload

---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopLab/SumNumbers/SumNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    internal class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int target=int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (sum < target)
+            {
+                sum+=int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
