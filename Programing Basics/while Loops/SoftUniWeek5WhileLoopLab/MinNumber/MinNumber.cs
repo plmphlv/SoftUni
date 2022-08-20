7bbdd861ff489496156b4bebf4b9ae38097b00e7
Plamen Pehlivanov <105740303+PlmPhlv@users.noreply.github.com>
2022-08-20 09:02:18 +0300

    Add files via upload

---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopLab/MinNumber/MinNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinNumber
+{
+    internal class MinNumber
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int min = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                int value = int.Parse(input);
+                if (value < min)
+                    min = value;
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
