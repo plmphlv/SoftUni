ba1ca705ae81ff747df0fa4b80a85a88d8e3b7a0
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2022-08-22 19:21:41 +0300

    Add files via upload

---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopLab/Graduation/Graduation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Graduation
+{
+    internal class Graduation
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grade = 1;
+            double gradeMarks = 0;
+            int failScore = 0;
+
+            while(grade<=12)
+            {
+                double yearMark =double.Parse(Console.ReadLine());
+
+                
+                if (yearMark<4.00)
+                {
+                    failScore++;
+                    if (failScore > 1)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {grade} grade");
+                        return;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                
+
+                grade++;
+                gradeMarks +=yearMark;
+            }
+
+            gradeMarks /= 12;
+
+            Console.WriteLine($"{name} graduated. Average grade: {gradeMarks:f2}");
+        }
+    }
+}
---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopLab/MaxNumber/MaxNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    internal class MaxNumber
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int max = int.MinValue;
+
+            while(input != "Stop")
+            {
+                int value=int.Parse(input);
+                if(value > max)
+                    max = value;
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
