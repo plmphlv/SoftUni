e26e326d55c5ebbbac7802e329bcbef01e37ebf2
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2022-08-22 19:22:29 +0300

    Add files via upload

---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopLab/AccountBalance/AccountBalance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    internal class AccountBalance
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double kinti = 0;
+            while (input != "NoMoreMoney")
+            {
+                double moneny=double.Parse(input);
+                if (moneny<0)
+                {
+                    Console.WriteLine($"Invalid operation!");
+                    Console.WriteLine($"Total: {kinti:f2}");
+                    return;
+                }
+                kinti += moneny;
+                Console.WriteLine($"Increase: {moneny:f2}");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {kinti:f2}");
+        }
+    }
+}
