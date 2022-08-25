282155549a3e65a0762a60c800f7df87d5378127
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2022-08-25 07:56:08 +0300

    Add files via upload

---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopExercise/EqualSumsEvenOddPosition/EqualSumsEvenOddPosition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EqualSumsEvenOddPosition
+{
+    internal class EqualSumsEvenOddPosition
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int finalNum = int.Parse(Console.ReadLine());
+
+            for (int currNum = firstNum; currNum <= finalNum; currNum++)
+            {
+                int current = currNum;
+                int digitPos = 6;
+                int equalSum = 0;
+                int oddSum = 0;
+                while (current > 0)
+                {
+                    int digit = current % 10;
+                    current=current / 10;
+
+                    if (digitPos % 2 == 0)
+                    {
+                        equalSum += digit;
+                    }
+                    else
+                    {
+                        oddSum += digit;
+                    }
+                    digitPos--;
+                }
+                if (oddSum == equalSum)
+                {
+                    Console.Write(currNum + " ");
+                }
+            }
+        }
+    }
+}
