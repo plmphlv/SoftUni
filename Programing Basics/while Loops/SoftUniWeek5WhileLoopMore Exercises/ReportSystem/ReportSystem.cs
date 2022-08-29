095774d49e780cf94080e8ebc92164c0912c196a
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2022-08-29 07:55:39 +0300

    Add files via upload

---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopMore Exercises/ReportSystem/ReportSystem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ReportSystem
+{
+    internal class ReportSystem
+    {
+        static void Main(string[] args)
+        {
+            int donationGoal = int.Parse(Console.ReadLine());
+            string command;
+            int transactionLine = 1;
+            int cashPayCount = 0;
+            double cashPaySum = 0;
+            int applePayCount = 0;
+            double applePaySum = 0;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int money = int.Parse(command);
+                if (transactionLine % 2 == 0) // card payments(applePay)
+                {
+                    if (money < 10)
+                    {
+                        Console.WriteLine("Error in transaction!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product sold!");
+                        applePayCount++;
+                        applePaySum += money;
+                    }
+                }
+                else //cash payments(cashPay)
+                {
+                    if (money > 100)
+                    {
+                        Console.WriteLine("Error in transaction!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product sold!");
+                        cashPayCount++;
+                        cashPaySum += money;
+                    }
+                }
+                transactionLine++;
+                if (cashPaySum + applePaySum >= donationGoal)
+                {
+                    Console.WriteLine($"Average CS: {cashPaySum / cashPayCount:f2}");
+                    Console.WriteLine($"Average CC: {applePaySum / applePayCount:f2}");
+                    return;
+                }
+            }
+            Console.WriteLine("Failed to collect required money for charity.");
+        }
+    }
+}
