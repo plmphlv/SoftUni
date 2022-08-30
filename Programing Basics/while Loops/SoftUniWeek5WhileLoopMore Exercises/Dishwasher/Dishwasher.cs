10e4ce6065aba91b3f86d352408f0527a90dfe40
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2022-08-30 07:54:38 +0300

    Add files via upload

---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopMore Exercises/Dishwasher/Dishwasher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Dishwasher
+{
+    internal class Dishwasher
+    {
+        static void Main(string[] args)
+        {
+            int bootle = int.Parse(Console.ReadLine());
+            int detergent = bootle * 750;
+            int dishes = 0; //чинии
+            int pans = 0; //тенджери
+            int count = 1;
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int washerLoad = int.Parse(command);
+
+                if (count % 3 == 0)
+                {
+                    detergent -= washerLoad * 15;
+                    if (detergent < 0)
+                    {
+                        Console.WriteLine($"Not enough detergent, {Math.Abs(detergent)} ml. more necessary!");
+                        return;
+                    }
+                    else
+                    {
+                        pans += washerLoad;
+                    }
+                }
+                else
+                {
+                    detergent -= washerLoad * 5;
+
+                    if (detergent < 0)
+                    {
+                        Console.WriteLine($"Not enough detergent, {Math.Abs(detergent)} ml. more necessary!");
+                        return;
+                    }
+                    else
+                    {
+                        dishes += washerLoad;
+                    }
+                }
+                count++;
+            }
+            Console.WriteLine($"Detergent was enough!");
+            Console.WriteLine($"{dishes} dishes and {pans} pots were washed.");
+            Console.WriteLine($"Leftover detergent {detergent} ml.");
+        }
+    }
+}
---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopMore Exercises/NumbersDividedBy3WithoutReminder/NumbersDividedBy3WithoutReminder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumbersDividedby3WithoutReminder
+{
+    internal class NumbersDividedby3WithoutReminder
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
