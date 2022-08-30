7cfe0f9768cc3966f532aec55fbb2daf4020011c
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2022-08-30 07:55:15 +0300

    Add files via upload

---
+++ Programing Basics/while Loops/SoftUniWeek5WhileLoopMore Exercises/AverageNumber/AverageNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfLoops = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 1; i <= numOfLoops; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine($"{sum / numOfLoops:f2}");
+        }
+    }
+}
