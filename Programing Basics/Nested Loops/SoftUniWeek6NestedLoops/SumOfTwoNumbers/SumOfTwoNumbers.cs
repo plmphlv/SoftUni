ebfb3e5ef9e82737a0a66efe351cb2d10c5ae5e0
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2022-09-03 13:54:46 +0300

    Add files via upload

---
+++ Programing Basics/Nested Loops/SoftUniWeek6NestedLoops/SumOfTwoNumbers/SumOfTwoNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    internal class SumOfTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            int intervalBeginning = int.Parse(Console.ReadLine());
+            int intervalEnd = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int magicNumberLock = 0;
+            int comboCount = 0;
+            bool found = false;
+            int storeI = 0;
+            int storeJ = 0;
+
+            for (int i = intervalBeginning; i <= intervalEnd; i++)
+            {
+                for (int j = intervalBeginning; j <= intervalEnd; j++)
+                {
+                    int sum = i + j;
+                    comboCount++;
+                    if (sum == magicNumber && !found)
+                    {
+                        storeI = i;
+                        storeJ = j;
+                        magicNumberLock = comboCount;
+                        found = true;
+                    }
+                }
+            }
+
+            if (found)
+                Console.WriteLine($"Combination N:{magicNumberLock} ({storeI} + {storeJ} = {magicNumber})");
+            else
+                Console.WriteLine($"{comboCount} combinations - neither equals {magicNumber}");
+        }
+    }
+}
---
+++ Programing Basics/Nested Loops/SoftUniWeek6NestedLoops/Travelling/Travelling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Travelling
+{
+    internal class Travelling
+    {
+        static void Main(string[] args)
+        {
+            string destination;
+
+            while ((destination = Console.ReadLine()) != "End")
+            {
+                double minSumReq = double.Parse(Console.ReadLine());
+                double savings = 0;
+                while (savings < minSumReq)
+                {
+                    double addFunds = double.Parse(Console.ReadLine());
+                    savings += addFunds;
+                }
+                Console.WriteLine($"Going to {destination}!");
+            }
+        }
+    }
+}
