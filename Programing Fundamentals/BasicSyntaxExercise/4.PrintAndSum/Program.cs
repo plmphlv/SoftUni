94dda7f3793b12beb8c2dbd4384027f9bb80a896
Plamen Pehlivanov <plmphlv@gmail.com>
2022-09-16 16:41:10 +0300

    Basic Syntax Exercise

---
+++ Programing Fundamentals/BasicSyntaxExercise/4.PrintAndSum/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _4.PrintAndSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            int sum =0;
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.Write($"Sum: {sum}");
+        }
+    }
+}
