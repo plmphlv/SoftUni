07006aface4d247374319bda4375c18acee83c3d
Plamen Pehlivanov <plmphlv@gmail.com>
2022-10-11 21:41:33 +0300

    More Exercises for Methods

--- Programing Fundamentals/04. Methods/MethodsMoreExercises/02.CenterPoint/CenterPoint.cs
+++ Programing Fundamentals/04. Methods/MethodsMoreExercises/02.CenterPoint/CenterPoint.cs
@@ -13,39 +13,26 @@ namespace _02.CenterPoint
                 points[i] = double.Parse(Console.ReadLine());
             }
 
-            double[] centerPoint = CenterPointFinder(points);
-            Console.WriteLine($"({String.Join(", ", centerPoint)})");
-
+            CenterPointFinder(points);
         }
 
-        static double[] CenterPointFinder(double[] points)
+        static void CenterPointFinder(double[] points)
         {
             double[] centerPoints = new double[2];
 
 
-            if (Math.Abs(points[0]) == Math.Abs(points[2]) && Math.Abs(points[1]) == Math.Abs(points[3]))
+            double first = Math.Sqrt(Math.Pow(points[1], 2) + Math.Pow(points[0], 2));
+            double secound = Math.Sqrt(Math.Pow(points[2], 2) + Math.Pow(points[3], 2));
+
+            if (first < secound)
             {
-                centerPoints[0] = points[0];
-                centerPoints[1] = points[1];
-            }
-            else if ((Math.Abs(points[0]) < Math.Abs(points[2])) && (Math.Abs(points[1]) < Math.Abs(points[3])))
-            {
-                centerPoints[0] = points[0];
-                centerPoints[1] = points[1];
-            }
-            else if ((Math.Abs(points[0]) <= Math.Abs(points[2])) || (Math.Abs(points[1]) <= Math.Abs(points[3])))
-            {
-                centerPoints[0] = points[0];
-                centerPoints[1] = points[1];
+                Console.WriteLine($"({points[0]}, {points[1]})");
             }
             else
             {
-                centerPoints[0] = points[2];
-                centerPoints[1] = points[3];
+                Console.WriteLine($"({points[2]}, {points[3]})");
             }
 
-            Math.Max(centerPoints[0], centerPoints[1]);
-            return centerPoints;
         }
     }
 }
