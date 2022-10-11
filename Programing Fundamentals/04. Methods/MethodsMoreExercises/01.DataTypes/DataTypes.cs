0d30e43f8104e956f2beca5868f0846a9af303c2
Plamen Pehlivanov <plmphlv@gmail.com>
2022-10-11 20:17:34 +0300

    More Exercises for Methods

---
+++ Programing Fundamentals/04. Methods/MethodsMoreExercises/01.DataTypes/DataTypes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01.DataTypes
+{
+    internal class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+
+            if (dataType == "int")
+            {
+                int dataInt = int.Parse(Console.ReadLine());
+                DataTupe(dataInt);
+            }
+            else if (dataType == "real")
+            {
+                double dataDouble = double.Parse(Console.ReadLine());
+                DataTupe(dataDouble);
+            }
+            else if (dataType == "string")
+            {
+                string str = Console.ReadLine();
+                DataTupe(str);
+            }
+        }
+
+        static void DataTupe(string dataType)
+        {
+            Console.WriteLine($"${dataType}$");
+        }
+        static void DataTupe(int dataType)
+        {
+            Console.WriteLine(dataType * 2);
+        }
+        static void DataTupe(double dataType)
+        {
+            Console.WriteLine($"{dataType * 1.5:f2}");
+        }
+    }
+}
---
+++ Programing Fundamentals/04. Methods/MethodsMoreExercises/02.CenterPoint/CenterPoint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _02.CenterPoint
+{
+    internal class CenterPoint
+    {
+        static void Main(string[] args)
+        {
+            double[] points = new double[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                points[i] = double.Parse(Console.ReadLine());
+            }
+
+            double[] centerPoint = CenterPointFinder(points);
+            Console.WriteLine($"({String.Join(", ", centerPoint)})");
+
+        }
+
+        static double[] CenterPointFinder(double[] points)
+        {
+            double[] centerPoints = new double[2];
+
+
+            if (Math.Abs(points[0]) == Math.Abs(points[2]) && Math.Abs(points[1]) == Math.Abs(points[3]))
+            {
+                centerPoints[0] = points[0];
+                centerPoints[1] = points[1];
+            }
+            else if ((Math.Abs(points[0]) < Math.Abs(points[2])) && (Math.Abs(points[1]) < Math.Abs(points[3])))
+            {
+                centerPoints[0] = points[0];
+                centerPoints[1] = points[1];
+            }
+            else if ((Math.Abs(points[0]) <= Math.Abs(points[2])) || (Math.Abs(points[1]) <= Math.Abs(points[3])))
+            {
+                centerPoints[0] = points[0];
+                centerPoints[1] = points[1];
+            }
+            else
+            {
+                centerPoints[0] = points[2];
+                centerPoints[1] = points[3];
+            }
+
+            Math.Max(centerPoints[0], centerPoints[1]);
+            return centerPoints;
+        }
+    }
+}
