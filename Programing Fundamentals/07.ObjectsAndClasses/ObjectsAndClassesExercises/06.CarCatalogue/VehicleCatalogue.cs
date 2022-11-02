2c0ab134f1a1cca91e4aa38bb7b87e459a9a29df
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2022-11-02 15:18:14 +0200

    Objects And Classes Exercises

---
+++ Programing Fundamentals/07.ObjectsAndClasses/ObjectsAndClassesExercises/06.CarCatalogue/VehicleCatalogue.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.CarCatalogue
+{
+    internal class VehicleCatalogue
+    {
+        static void Main(string[] args)
+        {
+
+            ProgramEngine engine = new ProgramEngine();
+
+        }
+
+    }
+
+    class ProgramEngine
+    {
+
+        public ProgramEngine()
+        {
+            Catalogue catalogue = new Catalogue();
+
+            VehicleMaker(catalogue);
+            BrowseCatalogue(catalogue);
+            CalculateAvarageHoursePower(catalogue);
+        }
+
+        public static void CalculateAvarageHoursePower(Catalogue catalogue)
+        {
+            double carAvgHP = catalogue.Cars.Average(x => x.VehicleHorsepower);
+            double truckAvgHP = catalogue.Trucks.Average(x=>x.VehicleHorsepower);
+
+            Console.WriteLine($"Cars have average horsepower of {carAvgHP:f2}.");
+            Console.WriteLine($"Trucks have average horsepower of {truckAvgHP:f2}.");
+        }
+
+        public static void BrowseCatalogue(Catalogue catalogue)
+        {
+            string printModel;
+            while ((printModel = Console.ReadLine()) != "Close the Catalogue")
+            {
+
+                Vehicle desiredVehicle = catalogue.Cars.Find(x => x.VehicleModel == printModel);
+
+                if (desiredVehicle == null)
+                {
+                    desiredVehicle = catalogue.Trucks.Find(x => x.VehicleModel == printModel);
+                }
+
+                desiredVehicle.PrintVehicle();
+            }
+        }
+
+        public static void VehicleMaker(Catalogue catalogue)
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string typeOfVehicle = CapitalizeWord(cmdArgs[0]);
+                string modelOfVehicle = cmdArgs[1];
+                string colourOfVehicle = cmdArgs[2];
+                int horsepowerOfVehicle = int.Parse(cmdArgs[3]);
+
+                switch (typeOfVehicle)
+                {
+                    case "Car":
+
+                        Vehicle car = new Vehicle(typeOfVehicle, modelOfVehicle, colourOfVehicle, horsepowerOfVehicle);
+                        catalogue.Cars.Add(car);
+
+                        break;
+                    case "Truck":
+
+                        Vehicle truck = new Vehicle(typeOfVehicle, modelOfVehicle, colourOfVehicle, horsepowerOfVehicle);
+                        catalogue.Trucks.Add(truck);
+
+                        break;
+                }
+
+            }
+        }
+
+        public static string CapitalizeWord(string word)
+        {
+            return String.Concat(char.ToUpper(word[0]), word.Substring(1));
+        }
+
+    }
+
+    class Catalogue
+    {
+
+        public Catalogue()
+        {
+            Cars = new List<Vehicle>();
+            Trucks = new List<Vehicle>();
+        }
+
+        public List<Vehicle> Cars { get; set; }
+        public List<Vehicle> Trucks { get; set; }
+
+    }
+
+    class Vehicle
+    {
+        public Vehicle(string vehicleType, string vehicleModel, string vehicleColour, int vehicleHorsepower)
+        {
+            VehicleType = vehicleType;
+            VehicleModel = vehicleModel;
+            VehicleColour = vehicleColour;
+            VehicleHorsepower = vehicleHorsepower;
+        }
+
+        public string VehicleType { get; set; }
+        public string VehicleModel { get; set; }
+        public string VehicleColour { get; set; }
+        public int VehicleHorsepower { get; set; }
+
+        public void PrintVehicle()
+        {
+
+            Console.WriteLine($"Type: {this.VehicleType}\r\n" +
+                $"Model: {this.VehicleModel}\r\n" +
+                $"Color: {this.VehicleColour}\r\n" +
+                $"Horsepower: {this.VehicleHorsepower}");
+
+        }
+
+    }
+
+}
---
+++ Programing Fundamentals/07.ObjectsAndClasses/ObjectsAndClassesExercises/07.OrderByAge/OrderByAge.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.OrderByAge
+{
+    internal class OrderByAge
+    {
+        static void Main(string[] args)
+        {
+            List<Person> ledger = new List<Person>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string name = cmdArgs[0];
+                int age = int.Parse(cmdArgs[1]);
+                int id = int.Parse(cmdArgs[2]);
+
+                Person person = ledger.FirstOrDefault(x => x.Id == id);
+
+                if (person == null)
+                {
+                    person = new Person(name, age, id);
+                    ledger.Add(person);
+                }
+                else
+                {
+                    person.EditPerson(name, age);
+                }
+
+            }
+
+            List<Person> orderedList = ledger.OrderBy(x => x.Age).ToList();
+
+            foreach (Person human in orderedList)
+            {
+                Console.WriteLine($"{human.Name} with ID: {human.Id} is {human.Age} years old.");
+            }
+
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, int id, int age)
+        {
+            Name = name;
+            Id = id;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+        public int Age { get; private set; }
+
+        public void EditPerson(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+}
