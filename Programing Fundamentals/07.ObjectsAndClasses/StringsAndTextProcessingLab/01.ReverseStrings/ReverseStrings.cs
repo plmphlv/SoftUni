a4d2a7827ec4c57b613f77694b85553168286d25
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2022-11-10 13:47:30 +0200

    Lab Strings and Text Processing
    WIP

---
+++ Programing Fundamentals/07.ObjectsAndClasses/StringsAndTextProcessingLab/01.ReverseStrings/ReverseStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.ReverseStrings
+{
+    internal class ReverseStrings
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
