e1f1f622df75264f46e22e49d24781245a2883f5
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2022-11-15 10:45:43 +0200

    Strings Processing
    WIP

--- Programing Fundamentals/09.StringsAndTextProcessing/StringsAndTextProcessingLab/01.ReverseStrings/ReverseStrings.cs
+++ Programing Fundamentals/09.StringsAndTextProcessing/StringsAndTextProcessingLab/01.ReverseStrings/ReverseStrings.cs
@@ -6,7 +6,18 @@ namespace _01.ReverseStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string command;
+            while ((command = Console.ReadLine())!="end")
+            {
+                string reversedWord =string.Empty;
+
+                for (int i = command.Length-1; i >= 0; i--)
+                {
+                    reversedWord += command[i];
+                }
+
+                Console.WriteLine($"{command} = {reversedWord}");
+            }
         }
     }
 }
---
+++ Programing Fundamentals/09.StringsAndTextProcessing/StringsAndTextProcessingLab/03.Substring/Substring.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.Substring
+{
+    internal class Substring
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
