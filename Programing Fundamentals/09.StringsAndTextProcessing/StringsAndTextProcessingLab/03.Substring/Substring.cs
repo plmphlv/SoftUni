8af946bd954abdc133d5a941195b656cb37c855f
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2022-11-16 18:47:33 +0200

    Strings and Text Processing
    Finished Strings and Text Processing Lab

--- Programing Fundamentals/09.StringsAndTextProcessing/StringsAndTextProcessingLab/03.Substring/Substring.cs
+++ Programing Fundamentals/09.StringsAndTextProcessing/StringsAndTextProcessingLab/03.Substring/Substring.cs
@@ -6,7 +6,16 @@ namespace _03.Substring
     {
         static void Main(string[] args)
         {
-            
+            string specialWord = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Length != -1)
+            {
+                int startIndex = text.IndexOf(specialWord);
+                text = text.Remove(startIndex, specialWord.Length);
+            }
+
+            Console.WriteLine(text);
         }
     }
 }
