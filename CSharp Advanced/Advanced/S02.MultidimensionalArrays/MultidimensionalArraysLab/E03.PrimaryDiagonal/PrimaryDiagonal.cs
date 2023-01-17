f7dccb34d422cfa3659b2c264a7c4c49f76b303a
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2023-01-17 23:49:19 +0200

    Repository reorganization

---
+++ CSharp Advanced/Advanced/S02.MultidimensionalArrays/MultidimensionalArraysLab/E03.PrimaryDiagonal/PrimaryDiagonal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace E03.PrimaryDiagonal
+{
+    internal class PrimaryDiagonal
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] cube = new int[size, size];
+
+            for (int i = 0; i < cube.GetLength(0); i++)
+            {
+                int[] matrixNums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int j = 0; j < cube.GetLength(1); j++)
+                {
+                    cube[i, j] = matrixNums[j];
+                }
+            }
+
+            int diagonalSum = 0;
+
+            for (int i = 0; i < cube.GetLength(0); i++)
+            {
+                diagonalSum += cube[i, i];
+            }
+
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}
---
+++ CSharp Advanced/Advanced/S02.MultidimensionalArrays/MultidimensionalArraysLab/E06.JaggedArrayModification/JaggedArrayModification.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace E06.JaggedArrayModification
+{
+    internal class JaggedArrayModification
+    {
+        static void Main(string[] args)
+        {
+            int jaggerDimmension = int.Parse(Console.ReadLine());
+
+            int[][] jagger = new int[jaggerDimmension][];
+
+            for (int row = 0; row < jaggerDimmension; row++)
+            {
+                jagger[row] = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
+            string command;
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                int yAxis = int.Parse(cmdArgs[1]);//row
+                int xAxis = int.Parse(cmdArgs[2]);//col
+
+                switch (cmdArgs[0])
+                {
+                    case "add":
+
+                        if (yAxis >= 0 && yAxis < jaggerDimmension && xAxis >= 0 && xAxis < jagger[yAxis].Length)
+                        {
+                            jagger[yAxis][xAxis] += int.Parse(cmdArgs[3]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid coordinates");
+                        }
+
+                        break;
+
+                    case "subtract":
+
+                        if (yAxis >= 0 && yAxis < jaggerDimmension && xAxis >= 0 && xAxis < jagger[yAxis].Length)
+                        {
+                            jagger[yAxis][xAxis] -= int.Parse(cmdArgs[3]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid coordinates");
+                        }
+
+                        break;
+                }
+            }
+
+            foreach (int[] i in jagger)
+            {
+                Console.WriteLine(String.Join(" ", i));
+            }
+        }
+    }
+}
