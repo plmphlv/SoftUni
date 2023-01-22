e87246720e7096b25a6b96f8cd8d6fa4e58d9999
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2023-01-22 21:04:46 +0200

    Started Streams, Files and Directories

--- CSharp Advanced/Advanced/S04.StreamsFilesAndDirectories/Skeleton-Lab/WordCount/WordCount.cs
+++ CSharp Advanced/Advanced/S04.StreamsFilesAndDirectories/Skeleton-Lab/WordCount/WordCount.cs
@@ -17,6 +17,48 @@
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
+            StreamReader reader = new StreamReader(wordsFilePath);
+            string[] words;
+            using (reader)
+            {
+                words = reader.ReadToEnd().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            reader = new StreamReader(textFilePath);
+            using (reader)
+            {
+                string[] splitters = { " ", ",", ", ", ".", "", ", ", "-", "!", "?!", "...", "?", "\r\n" };
+                string[] elements = reader.ReadToEnd().ToLower().Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+
+                StreamWriter writer = new StreamWriter(outputFilePath);
+                Dictionary<string, int> wordLogs = new Dictionary<string, int>();
+
+                foreach (string keyWord in words)
+                {
+                    int count = 0;
+
+                    foreach (string item in elements)
+                    {
+                        if (keyWord == item)
+                        {
+                            count++;
+                        }
+                    }
+                    wordLogs[keyWord] = count;
+                }
+
+                wordLogs = wordLogs.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+                using (writer)
+                {
+                    foreach (var item in wordLogs)
+                    {
+                        writer.WriteLine($"{item.Key} - {item.Value}");
+                    }
+                }
+            }
+
+
         }
     }
 }
