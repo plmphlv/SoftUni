a359f2796d21c6ec38c5a2d21953f13b4c29a0f7
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2023-01-30 22:26:11 +0200

    Started Defining Classes and Namespaces Lab

---
+++ CSharp Advanced/Advanced/S06.DefiningClasses/DefiningClassesLab/E02.CarExtension/Car.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+            set
+            {
+                make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+        public double FuelQuantity
+        {
+            get
+            {
+                return fuelQuantity;
+            }
+            set
+            {
+                fuelQuantity = value;
+            }
+        }
+        public double FuelConsumption
+        {
+            get
+            {
+                return fuelConsumption;
+            }
+            set
+            {
+                fuelConsumption = value;
+            }
+        }
+
+        public void Drive(double distance)
+        {
+            double neededFuel = fuelConsumption * distance;
+            if (fuelQuantity - neededFuel <= 0)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+                return;
+            }
+
+            fuelQuantity -= neededFuel;
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder me = new StringBuilder();
+
+            me.AppendLine($"Make: {this.make}");
+            me.AppendLine($"Model: {this.model}");
+            me.AppendLine($"Year: {this.year}");
+            me.AppendLine($"Fuel: {this.fuelQuantity:f2}");
+
+            return me.ToString();
+        }
+    }
+}
