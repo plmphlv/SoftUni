486a84353d255d7d91237732ae3e273336e94079
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2023-03-20 12:25:35 +0200

    Finished Reflections

---
+++ CSharp Advanced/OOP/S03.Polymorphism/ReflectionAndAttributesLab/E06.CodeTracker/AuthorAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    internal class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ CSharp Advanced/OOP/S03.Polymorphism/ReflectionExercise/ValidationAttributes/Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ValidationAttributes.Attributes;
+
+public class MyRequiredAttribute : MyValidationAttribute
+{
+    public override bool IsValid(object obj)
+        => obj is not null;
+}
