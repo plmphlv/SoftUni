93b5776b1a9a168ceb70743583c2474b77517201
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2023-06-21 10:13:23 +0300

    Code optimisations

--- JS Front-End/JavaScript/01.JSSyntaxFundamentals/FundamentalsLab/E07.TheatrePromotions.js
+++ JS Front-End/JavaScript/01.JSSyntaxFundamentals/FundamentalsLab/E07.TheatrePromotions.js
@@ -13,7 +13,7 @@ function tickets(dayOfWeek, age) {
       break;
     case "Weekend":
       if (age >= 0 && age <= 18) {
-        console.log("18$");
+        console.log("15$");
       } else if (age > 18 && age <= 64) {
         console.log("20$");
       } else if (age > 64 && age <= 122) {
---
+++ JS Front-End/JavaScript/01.JSSyntaxFundamentals/FundamentalsLab/E09.NumbersFrom1To5.js
@@ -0,0 +1,7 @@
+function dercament() {
+  for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+}
+
+dercament();
