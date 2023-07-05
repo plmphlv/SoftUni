ff7b2efbefbd1bd2190e09df08ca436c0afd0d1d
Plamen Pehlivanov <plamen.rp199@yahoo.com>
2023-07-05 15:46:21 +0300

    Finished Functions And Statements Lab

---
+++ JS Front-End/JavaScript/02.FunctionsAndStatements/FunctionsAndStatementsLab/E06.SignCheck.js
@@ -0,0 +1,19 @@
+function  signCheck(num1,num2,num3) {
+    const numArr=[num1,num2,num3];
+    let negattive=0;
+
+    for (const iterator of numArr) {
+        if(iterator<0){
+            negattive++;
+        }
+    }
+
+    if(negattive===0 || negattive===2){
+        console.log("Positive");
+    }
+    else{
+        console.log("Negative");
+    }
+}
+
+signCheck(-5, -12, 15);
